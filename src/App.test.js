8cf3559dc3db8e45753f3c40bb13081dd4284ae6
Carlos Patino <carlospatinos@gmail.com>
2020-10-22 12:05:28 +0100

    mend

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('verify the app renders correctly', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
